3ab7e538042bfc2fb7999100d5c6d7fb2a37bb7d
Vicens274 <cinjeuf41@gmail.com>
2024-05-09 11:27:06 +0200

    bbdd

--- añadir/check.php
+++ añadir/check.php
@@ -1,6 +1,6 @@
 <?php
 
-    $con = mysqli_connect('localhost', 'vicente', 'abc123.', 'isadmins');
+    $con = mysqli_connect('localhost', 'id22057369_vicente', '#Calvar96', 'id22057369_isadmins');
 
     if($con == false) {
         die('Error: No se puede conectar a la base de datos');
