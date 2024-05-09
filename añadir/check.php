16fe50a4ffb6b2a324fb6b9a982d8c018a20fd13
Vicens274 <cinjeuf41@gmail.com>
2024-05-09 13:08:36 +0200

    123

--- añadir/check.php
+++ añadir/check.php
@@ -1,6 +1,7 @@
 <?php
 
-    $con = mysqli_connect('localhost', 'id22057369_vicente', '#Calvar96', 'id22057369_isadmins');
+   // $con = mysqli_connect('localhost', 'id22057369_vicente', '#Calvar96', 'id22057369_isadmins');
+    $con = mysqli_connect('localhost', 'vicente', 'abc123.', 'isadmins');
 
     if($con == false) {
         die('Error: No se puede conectar a la base de datos');
--- añadir/login.php
+++ añadir/login.php
@@ -1,26 +1,31 @@
 <?php
+include('check.php');
+if(isset($_POST['submit'])) {
+    $user  = $_POST['username'];
+    $password = $_POST['password'];
 
-    include('check.php');
-    if(isset($_POST['submit'])) {
-        $user  = $_POST['username'];
-        $password = $_POST['password'];
+    $user = stripslashes($user);
+    $password = stripslashes($password);
 
-        $user = stripslashes($user);
-        $password = stripslashes($password);
+    $user = mysqli_real_escape_string($con, $user);
+    $password = mysqli_real_escape_string($con, $password);
 
-        $user = mysqli_real_escape_string($con, $user);
-        $password = mysqli_real_escape_string($con, $password);
-
-        $query = "SELECT * FROM administradores WHERE Usuario ='$user' AND Contraseña ='$password'";
-        $result = mysqli_query($con, $query);
-        
-        if($result->num_rows > 0) {
-            header('Location: ../dashboard.html');
+    $query = "SELECT * FROM administradores WHERE Usuario ='$user' AND Contraseña ='$password'";
+    $result = mysqli_query($con, $query);
+    
+    if($result->num_rows > 0) {
+        // Obtener el rol del usuario
+        $row = $result->fetch_assoc();
+        $rol = $row['rol'];
+        if ($rol == 'superadministrador') {
+            header('Location: ../dashboard.html'); // Redirigir a dashboard para superadministradores
+        } elseif ($rol == 'administrador') {
+            header('Location: ../dashboard.html'); // Redirigir a categorias para administradores
         } else {
-            header('Location: ../categorias.html');           
-            $response = "Error: Credenciales incorrectas";
+            header('Location: ../categorias.html'); // Redirigir a página de inicio de sesión si el rol no está definido o es otro
         }
+    } else {
+        header('Location: ../categorias.html'); // Redirigir a página de inicio de sesión si las credenciales son incorrectas
     }
-
-    echo $response;
+}
 ?>
