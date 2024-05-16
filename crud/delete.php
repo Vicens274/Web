d736ce8ddcdc0dce36418878a37eff74878a2f03
Vicens274 <cinjeuf41@gmail.com>
2024-05-16 10:08:24 +0200

    16/05

--- crud/delete.php
+++ crud/delete.php
@@ -1,49 +1,46 @@
 <?php
 // Process delete operation after confirmation
-if(isset($_POST["id"]) && !empty($_POST["id"])){
+if(isset($_POST["Usuario"]) && !empty($_POST["Usuario"])){
     // Include config file
     require_once "config.php";
     
     // Prepare a delete statement
-    $sql = "DELETE FROM employees WHERE id = :id";
+    $sql = "DELETE FROM administradores WHERE Usuario = :usuario";
+
     
     if($stmt = $pdo->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bindParam(":id", $param_id);
+        $stmt->bindParam(":usuario", $param_usuario);
         
+
         // Set parameters
-        $param_id = trim($_POST["id"]);
+        $param_usuario = trim($_POST["Usuario"]);
         
         // Attempt to execute the prepared statement
         if($stmt->execute()){
             // Records deleted successfully. Redirect to landing page
-            header("location: index.php");
+            header("location: crud.php?Usuario=" . urlencode($_POST["Usuario"]));
             exit();
         } else{
             echo "Oops! Something went wrong. Please try again later.";
+            print_r($stmt->errorInfo());
         }
     }
      
+
     // Close statement
     unset($stmt);
     
     // Close connection
     unset($pdo);
-} else{
-    // Check existence of id parameter
-    if(empty(trim($_GET["id"]))){
-        // URL doesn't contain id parameter. Redirect to error page
-        header("location: error.php");
-        exit();
-    }
-}
+} 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Delete Record</title>
+    <title>Eliminar Administrador</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .wrapper{
@@ -57,14 +54,14 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="mt-5 mb-3">Delete Record</h2>
+                    <h2 class="mt-5 mb-3">Eliminar Administrador</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger">
-                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                            <p>Are you sure you want to delete this employee record?</p>
+                        <input type="hidden" name="Usuario" value="<?php echo isset($_POST["Usuario"]) ? htmlspecialchars($_POST["Usuario"]) : ''; ?>"/>
+                            <p>Seguro que deseas eliminar este administrador de la base de datos?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-secondary ml-2">No</a>
+                                <a href="crud.php" class="btn btn-secondary ml-2">No</a>
                             </p>
                         </div>
                     </form>
